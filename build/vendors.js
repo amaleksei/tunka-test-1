48f5adf096d0b529d1aebeb7de3fa85ca383840a
Aleksei <amaleksei@MacBook-Pro-Aleksei.local>
2019-10-15 14:25:17 +0800

    Fix view

--- build/vendors.js
+++ build/vendors.js
@@ -1,5 +1,30 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([["vendors~main"],{
 
+/***/ "../../../../../../../../../../../../../Applications/Framer X.app/Contents/Resources/Server/lib/framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-macbook-pro/build/index.js":
+/*!********************************************************************************************************************************************************************!*\
+  !*** /Applications/Framer X.app/Contents/Resources/Server/lib/framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-macbook-pro/build/index.js ***!
+  \********************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+class DeviceSkin {
+    constructor() {
+        this.image = "device.png";
+        this.imageWidth = 3820;
+        this.imageHeight = 2320;
+        this.padding = 40;
+        this.background = "black";
+    }
+}
+exports.DeviceSkin = DeviceSkin;
+//# sourceMappingURL=index.js.map
+
+
+/***/ }),
+
 /***/ "../../../../../../../../../../../../../Applications/Framer X.app/Contents/Resources/Server/node_modules/process/browser.js":
 /*!********************************************************************************************!*\
   !*** /Applications/Framer X.app/Contents/Resources/Server/node_modules/process/browser.js ***!
